616e30549ab4ccf50b6f322700942825a4c06732
zhouhuafei <zhouhuafei@shopex.cn>
2018-01-16 15:38:49 +0800

    1.0.13

--- index.test.js
+++ index.test.js
@@ -1,19 +1,25 @@
 const tools = require('./index');
+
 test(`tools.fillZero(9, 2) // '09'`, () => {
     expect(tools.fillZero(9, 2)).toBe('09');
 });
+
 test(`tools.fillZero(9, 3) // '009'`, () => {
     expect(tools.fillZero(9, 3)).toBe('009');
 });
+
 test(`tools.typeOf(123); // 'number'`, () => {
     expect(tools.typeOf(123)).toBe('number');
 });
+
 test(`tools.keepDecimal(1000, 3); // '1,000.000'`, () => {
     expect(tools.keepDecimal(1000, 3)).toBe('1,000.000');
 });
+
 test(`tools.keepDecimal(1000, 3, false); // '1000.000'`, () => {
     expect(tools.keepDecimal(1000, 3, false)).toBe('1000.000');
 });
+
 test(
     `
     tools.extend({
@@ -23,10 +29,8 @@ test(
     `,
     () => {
         expect(tools.extend({
-            defaults: { a: 1, c: [1, 2] },
-            inherits: { b: 2, c: [3] },
-        })).toEqual({ a: 1, b: 2, c: [3, 2] });
+            defaults: {a: 1, c: [1, 2]},
+            inherits: {b: 2, c: [3]},
+        })).toEqual({a: 1, b: 2, c: [3, 2]});
     }
 );
-
-tools.typeOf(123); // 'number'
