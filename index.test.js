ece73bc7855f1be6cdf026e46170c5c556eb16f3
zhouhuafei <zhouhuafei@shopex.cn>
2018-01-16 13:41:30 +0800

    jest

--- index.test.js
+++ index.test.js
@@ -14,14 +14,17 @@ test(`tools.keepDecimal(1000, 3); // '1,000.000'`, () => {
 test(`tools.keepDecimal(1000, 3, false); // '1000.000'`, () => {
     expect(tools.keepDecimal(1000, 3, false)).toBe('1000.000');
 });
-test(`tools.extend({
+test(
+    `tools.extend({
         defaults: { a: 1, c: [1, 2] },
         inherits: { b: 2, c: [3] },
-    }) // { a: 1, b: 2, c: [3, 2] }`, () => {
-    expect(tools.extend({
-        defaults: { a: 1, c: [1, 2] },
-        inherits: { b: 2, c: [3] },
-    })).toEqual({ a: 1, b: 2, c: [3, 2] });
-});
+    }) // { a: 1, b: 2, c: [3, 2] }`,
+    () => {
+        expect(tools.extend({
+            defaults: { a: 1, c: [1, 2] },
+            inherits: { b: 2, c: [3] },
+        })).toEqual({ a: 1, b: 2, c: [3, 2] });
+    }
+);
 
 tools.typeOf(123); // 'number'
